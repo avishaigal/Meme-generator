355ff3821056962ec226c027f54e5814a50f2c9b
avishaigal <avishai.gal@gmail.com>
2025-07-16 11:57:56 +0300

    Create img folder add render img textbox

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 'use strict'
+
+var gElCanvas
+var gCtx
+var gMeme
+
+// memeController
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
+
+function renderMeme() {
+    const elImg = document.querySelector('img')
+    gElCanvas.height = (elImg.naturalHeight / elImg.naturalWidth) * gElCanvas.width
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+
+
+    // textbox
+    const elTextInput = document.querySelector('.canvas-text')
+    const text = elTextInput.value || 'Text Box'
+
+    gCtx.lineWidth = 1
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'black'
+    gCtx.font = '40px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+    gCtx.fillText(text, 150, 50)
+    gCtx.strokeText(text, 150, 50)
+}
+
+
+// memeService
+
